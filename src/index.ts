e36665f37951cb104463788e95b1ee2cbf041887
Simon <simon.hessel@kreios.lu>
2021-11-26 15:45:27 +0100

    Added implementation

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,41 @@
-import { exportVariable, setFailed } from "@actions/core";
+import { getInput, setFailed, setOutput } from "@actions/core";
 
-const setEnvironmentVariable = (key: string, value: string) => {
-  exportVariable(key, value);
+const setOutputWhenPrefixed = (
+  env: string,
+  key: string,
+  value: string
+): boolean => {
+  const prefix = `${env}_`;
+  if (key.startsWith(prefix)) {
+    setOutput(key.slice(prefix.length), value);
+    return true;
+  }
+  return false;
 };
 
-(() => {
+const main = () => {
   try {
+    const data: Record<string, string> = JSON.parse(
+      getInput("secrets", { required: true })
+    );
+    const environment = getInput("environment", {
+      required: false,
+      trimWhitespace: true,
+    }).toLocaleUpperCase();
+    const globalPrefix = getInput("globalPrefix", {
+      required: false,
+      trimWhitespace: true,
+    }).toLocaleUpperCase();
+
+    Object.entries(data).forEach(
+      ([key, value]) =>
+        setOutputWhenPrefixed(environment, key, value) ||
+        setOutputWhenPrefixed(globalPrefix, key, value)
+    );
   } catch (error) {
     setFailed((error as Error).message);
     process.exit(1);
   }
-})();
+};
+
+main();
